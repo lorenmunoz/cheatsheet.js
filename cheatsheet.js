0c07df818e2265e478347b881b3431a5cbb1f32f
loraine munoz <loren1626@hotmail.com>
2016-01-05 17:48:11 -0500

    added functions

--- cheatsheet.js
+++ cheatsheet.js
@@ -41,3 +41,27 @@ console.log(mudslide[2]);
 var screwDriver = {spirit: 'vodka', mix: 'orange'};
 
 console.log(screwDriver.spirit);
+
+
+// FUNCTIONS
+
+function margaritaIngredients() {
+  var	margarita = prompt("Do you know what's in a margarita?")
+  return margarita + "? ok, Tequila, Triple Sec, Sour Mix, limejuice.";
+}
+
+alert( margaritaIngredients() );
+
+function blackRussianIngredients() {
+	var blackRussian = prompt("How about in a Black Russian Cocktail?")
+  return blackRussian + "? Vodka and Kalhua, now you can be a bartender!!!   "
+} 
+
+alert( blackRussianIngredients() ); 	
+
+function ManhattanIngredients() {
+  var manhattan = prompt("Final question " + userName + ". Can you mix a Manhattan?")
+  return manhattan + "?! it is Whiskey, Sweet Vermouth and Bitters!"
+}
+
+alert( ManhattanIngredients() );
